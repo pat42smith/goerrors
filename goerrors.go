1a44587c7fd533a72190f4f0bfb645163f56a6b7
Patrick Smith <pat42smith@gmail.com>
2022-08-26 15:22:43 -0700

    Added NilOrExitX.

--- goerrors.go
+++ goerrors.go
@@ -4,6 +4,11 @@
 // Package goerrors provides a few simple functions for handling errors in Go.
 package goerrors
 
+import (
+	"fmt"
+	"os"
+)
+
 // NilOrPanic panics with e if e is not nil.
 func NilOrPanic(e error) {
 	if e != nil {
@@ -11,14 +16,34 @@ func NilOrPanic(e error) {
 	}
 }
 
-// NilOrPanic1 panics with e if e is not nil. Otherwise, it returns t.
+// NilOrPanic1 is like NilOrPanic, but returns t when e is nil.
 func NilOrPanic1[T any](t T, e error) T {
 	NilOrPanic(e)
 	return t
 }
 
-// NilOrPanic2 panics with e if e is not nil. Otherwise, it returns (t1, t2).
+// NilOrPanic2 is like NilOrPanic, but returns (t1, t2) when e is nil.
 func NilOrPanic2[T1, T2 any](t1 T1, t2 T2, e error) (T1, T2) {
 	NilOrPanic(e)
+	return t1, t2
+}
+
+// If e is not nil, NilOrExit prints e to stderr and terminates the process.
+func NilOrExit(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e)
+		os.Exit(1)
+	}
+}
+
+// NilOrExit1 is like NilOrExit, but returns t when e is nil.
+func NilOrExit1[T any](t T, e error) T {
+	NilOrExit(e)
+	return t
+}
+
+// NilOrExit2 is like NilOrExit, but returns (t1, t2) when e is nil.
+func NilOrExit2[T1, T2 any](t1 T1, t2 T2, e error) (T1, T2) {
+	NilOrExit(e)
 	return t1, t2
 }
--- goerrors_test.go
+++ goerrors_test.go
@@ -5,6 +5,10 @@ package goerrors
 
 import (
 	"fmt"
+	"io"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/pat42smith/gotest"
@@ -52,4 +56,50 @@ func TestNilOrPanic2(t *testing.T) {
 	u, v = NilOrPanic2(foo())
 	gotest.Expect(t, x, u)
 	gotest.Expect(t, y, v)
+}
+
+func TestNilOrExit(t *testing.T) {
+	NilOrExit(nil)
+}
+
+func TestNilOrExit1(t *testing.T) {
+	var x uint16 = 331
+	gotest.Expect(t, x, NilOrExit1(x, nil))
+}
+
+func TestNilOrExit2(t *testing.T) {
+	x := true
+	y := "never"
+	u, v := NilOrPanic2(x, y, nil)
+	gotest.Expect(t, x, u)
+	gotest.Expect(t, y, v)
+}
+
+func TestExits(t *testing.T) {
+	tmp := t.TempDir()
+	testexits := filepath.Join(tmp, "testexits.exe")
+	gocmd := NilOrPanic1(exec.LookPath("go"))
+	cmd := exec.Command(gocmd, "build", "-o", testexits, "testdata/testexits.go")
+	out, e := cmd.CombinedOutput()
+	if e != nil || len(out) > 0 {
+		t.Log(string(out))
+		t.Fatal(e)
+	}
+
+	for _, s := range []string{"0 zero\n", "1 one\n", "2 two\n"} {
+		arg, result, _ := strings.Cut(s, " ")
+		cmd = exec.Command(testexits, arg)
+		cmd.Stdin = nil
+		out := NilOrPanic1(cmd.StdoutPipe())
+		err := NilOrPanic1(cmd.StderrPipe())
+		NilOrPanic(cmd.Start())
+		gotest.Expect(t, 0, len(NilOrPanic1(io.ReadAll(out))))
+		gotest.Expect(t, result, string(NilOrPanic1(io.ReadAll(err))))
+		switch e := cmd.Wait().(type) {
+		case *exec.ExitError:
+			gotest.Expect(t, 1, e.ExitCode())
+		default:
+			t.Fatal(e)
+		}
+	}
 }
